efef3e24563ccb35712a61ed73d43712851577a5
Gabriel Valenzuela Lorca <gabriel.valenzuela.lorca@gmail.com>
2021-10-01 20:42:50 -0300

    Replace js dom managment with use of useRef

--- src/components/StoryForm.js
+++ src/components/StoryForm.js
@@ -6,17 +6,18 @@ import { newStory } from '../utils/functions'
 const StoryForm = ({isActive, handleClose}) => {  
   const stories = useSelector(state => state.stories);
   const dispatch = useDispatch();
-  const storyForm = useRef(); 
-  const fields = ["numero","titulo","descripcion"];
+  const formRef = useRef({}); 
+  const warningRef = useRef({});
 
   const save = () => {
     let result = {};
     let validation = true;
 
-    fields.forEach(field => {
-      result[field] = storyForm.current.elements[field].value
-      const inputClass = storyForm.current.elements[field].classList;
-      const warningMessageClass = storyForm.current.querySelector(`.warning-${field}`).classList;
+    Object.keys(formRef.current).forEach(field => {
+      const element = formRef.current[field];
+      result[field] = element.value;
+      const inputClass = element.classList;
+      const warningMessageClass = warningRef.current[field].classList;
 
       if(result[field] === ""){
         inputClass.add('is-danger')
@@ -44,8 +45,8 @@ const StoryForm = ({isActive, handleClose}) => {
   }
 
   const clearForm = () => {
-    fields.forEach((field) => {
-      storyForm.current.elements[field].value = "";
+    Object.keys(formRef.current).forEach(field => {
+      formRef.current[field].value = "";
     });
 
     handleClose();
@@ -62,7 +63,7 @@ const StoryForm = ({isActive, handleClose}) => {
           </p>
         </header>
 
-        <form className="modal-card-body" ref={storyForm}>
+        <section className="modal-card-body">
           <div className="field">
             <label className="label">Número Historia</label>
             <div className="control">
@@ -72,29 +73,35 @@ const StoryForm = ({isActive, handleClose}) => {
                 </div>
 
                 <div className="control">
-                  <input className="input" name="numero" type="number" placeholder="10" min="0" />
+                  <input ref={el => formRef.current.numero = el} className="input" type="number" placeholder="10" min="0" />
                 </div>
               </div>
             </div>
-            <p className="help is-danger is-hidden warning-numero">Este campo es obligatorio</p>
+            <p ref={el => warningRef.current.numero = el} className="help is-danger is-hidden">
+              Este campo es obligatorio
+            </p>
           </div>
 
           <div className="field">
             <label className="label">Título Historia</label>
             <div className="control">
-              <input className="input" name="titulo" type="text" placeholder="Creación de usuarios, CRUD perfiles, etc..."/>
+              <input ref={el => formRef.current.titulo = el} className="input" type="text" placeholder="Creación de usuarios, CRUD perfiles, etc..."/>
             </div>
-            <p className="help is-danger is-hidden warning-titulo">Este campo es obligatorio</p>
+            <p ref={el => warningRef.current.titulo = el} className="help is-danger is-hidden">
+              Este campo es obligatorio
+            </p>
           </div>
 
           <div className="field">
             <label className="label">Descripción Historia</label>
             <div className="control">
-              <textarea className="textarea" name="descripcion" placeholder="Como <sujeto> quiero <deseo> para <objetivo>..."/>
+              <textarea ref={el => formRef.current.descripcion = el} className="textarea" placeholder="Como <sujeto> quiero <deseo> para <objetivo>..."/>
             </div>
-            <p className="help is-danger is-hidden warning-descripcion">Este campo es obligatorio</p>
+            <p ref={el => warningRef.current.descripcion = el} className="help is-danger is-hidden">
+              Este campo es obligatorio
+            </p>
           </div>
-        </form>
+        </section>
 
         <footer className="modal-card-foot">
           <button className="button is-success" onClick={save}>
