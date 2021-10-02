c1d6ada3bd4f385bcc38a24ece68712903c06960
Gabriel Valenzuela Lorca <gabriel.valenzuela.lorca@gmail.com>
2021-10-02 00:27:38 -0300

    Fix bug of modal with draggables

--- src/components/CardDraggable.js
+++ src/components/CardDraggable.js
@@ -8,55 +8,57 @@ const CardDraggable = ({item}) => {
   const [modalState, setModal] = useState(false);
 
   return (
-    <Draggable draggableId={item.id.toString()} index={item.index}>
-      {(provided, snapshot) => (
-        <article className={`card mb-3 has-background-${snapshot.isDragging ? "grey-light" : "light"}`}
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <div className="card-header">
-            <p className="card-header-title">
-                HU{item.numero} - {item.titulo}
-            </p>
+    <div>
+      <Draggable draggableId={item.id.toString()} index={item.index}>
+        {(provided, snapshot) => (
+          <article className={`card mb-3 has-background-${snapshot.isDragging ? "grey-light" : "light"}`}
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <div className="card-header">
+              <p className="card-header-title">
+                  HU{item.numero} - {item.titulo}
+              </p>
 
-            <button className="mt-1 mr-1 button is-light is-rounded" 
-              onClick={() => setModalState(true, setModal)}
-            >
-              <span className="icon" >
-                <i className="fas fa-lg fa-ellipsis-v"></i>
-              </span>
-            </button>
-          </div>
-
-          <div className="card-content py-2">
-            <progress className="progress is-link mb-2" value={item.avance.toString()} max="100"/>
-
-            <span className="icon-text level mb-0">
-              <span className={`icon level-left has-text-${criticidadStyle[item.criticidad]}`}
+              <button className="mt-1 mr-1 button is-light is-rounded" 
+                onClick={() => setModalState(true, setModal)}
               >
-                <i className="fas fa-circle"></i>
+                <span className="icon" >
+                  <i className="fas fa-lg fa-ellipsis-v"></i>
+                </span>
+              </button>
+            </div>
+
+            <div className="card-content py-2">
+              <progress className="progress is-link mb-2" value={item.avance.toString()} max="100"/>
+
+              <span className="icon-text level mb-0">
+                <span className={`icon level-left has-text-${criticidadStyle[item.criticidad]}`}
+                >
+                  <i className="fas fa-circle"></i>
+                </span>
+                
+                <span className="level-right tag is-primary is-rounded">{item.puntos} Ptos.</span>
               </span>
-              
-              <span className="level-right tag is-primary is-rounded">{item.puntos} Ptos.</span>
-            </span>
 
-            <span className="has-text-weight-medium">Responsables:</span>
+              <span className="has-text-weight-medium">Responsables:</span>
 
-            <ul className="pl-2">
-              {item.responsables.length ?
-                item.responsables.map((responsable, index) => 
-                  <li className="is-size-7" key={index}>{responsable}</li>
-                ) :  
-                <li className="is-size-7">Sin Responsables</li>
-              }
-            </ul>
-          </div>
+              <ul className="pl-2">
+                {item.responsables.length ?
+                  item.responsables.map((responsable, index) => 
+                    <li className="is-size-7" key={index}>{responsable}</li>
+                  ) :  
+                  <li className="is-size-7">Sin Responsables</li>
+                }
+              </ul>
+            </div>
+          </article>
+        )}
+      </Draggable>
 
-          <StoryDetails story={item} isActive={modalState} closeModal={() => setModalState(false, setModal)}/>
-        </article>
-      )}
-    </Draggable>
+      <StoryDetails story={item} isActive={modalState} closeModal={() => setModalState(false, setModal)}/>
+    </div>
   )
 }
 
