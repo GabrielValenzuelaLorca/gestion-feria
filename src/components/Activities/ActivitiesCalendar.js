48c489cccebfc18876e4c2f0979e2d748629dfc0
Gabriel Valenzuela <gabriel.valenzuela.lorca@gmail.com>
2022-05-24 02:48:28 -0400

    Change css folder to sass folder

--- src/components/Activities/ActivitiesCalendar.js
+++ src/components/Activities/ActivitiesCalendar.js
@@ -3,7 +3,7 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from "@fullcalendar/interaction";
 import esLocale from '@fullcalendar/core/locales/es';
-import '../../css/calendar.sass'
+import '../../sass/calendar.sass'
 import ActivityDetails from './ActivityDetails';
 import { setModalState } from '../../utils/functions';
 
