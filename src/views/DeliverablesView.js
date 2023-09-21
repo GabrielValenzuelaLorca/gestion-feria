0c5233eb77b15a391c81d24676f02d91385e75b3
Gabriel Valenzuela <gabriel.valenzuela.lorca@hotmail.com>
2023-09-20 23:22:30 -0300

    Add map of activities to deliverables

--- src/views/DeliverablesView.js
+++ src/views/DeliverablesView.js
@@ -1,61 +1,83 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { getActivities } from "../services/activity";
+import useFetch from "../hooks/useFetch";
+import { diffDates } from "../utils/functions";
 
 const DeliverablesView = () => {
-  return (
-    <section className= "section pb-0">
-      <div className='box'>
-        <div className='level'>
-          <div className='level-left'>
-            <div>
-              <h1 className='title'>Plan de Proyecto</h1>
-              <p>Aquí iría una descripción muy buena</p>
-              <p>Fecha de cierre: 20/01/2022</p>
-              <p>Tiempo restante: 2 días</p>
-            </div>
-          </div>
-          <div className='level-right'>
-            <div>
-              <div className='block'>
+  const [activitiesState, setActivities] = useState([]);
+  const [fetchActivities, isLoadingActivities] = useFetch(
+    getActivities,
+    setActivities
+  );
 
-                <div className="file has-name is-boxed">
-                  <label className="file-label">
-                    <input className="file-input" type="file" name="resume"/>
-                    <span className="file-cta">
-                      <span className="file-icon">
-                        <i className="fas fa-upload"></i>
+  useEffect(() => {
+    fetchActivities();
+  }, [fetchActivities]);
+  return (
+    <section className="section pb-0">
+      {isLoadingActivities ? (
+        <progress className="progress is-primary" />
+      ) : activitiesState.length ? (
+        activitiesState.map((activity) => {
+          return (
+            <div key={activity.id} className="box columns">
+              <div className="column">
+                <h1 className="title">{activity.name}</h1>
+                <p className="subtitle">{activity.description}</p>
+                <p>
+                  Fecha de término:{" "}
+                  {activity.end.split("-").reverse().join("/")}
+                </p>
+                <p>
+                  Tiempo restante: {diffDates(new Date(), activity.end)} día(s)
+                </p>
+              </div>
+              <div className="column is-narrow">
+                <div className="block">
+                  <div className="file has-name is-boxed">
+                    <label className="file-label">
+                      <input className="file-input" type="file" name="file" />
+                      <span className="file-cta">
+                        <span className="file-icon">
+                          <i className="fas fa-upload"></i>
+                        </span>
+                        <span className="file-label">Seleccionar archivo</span>
                       </span>
-                      <span className="file-label">
-                        Seleccionar archivo
+                      <span className="file-name">
+                        Screen Shot 2017-07-29 at 15.54.25.png
                       </span>
-                    </span>
-                    <span className="file-name">
-                      Screen Shot 2017-07-29 at 15.54.25.png
-                    </span>
-                  </label>
+                    </label>
+                  </div>
+                </div>
+                <div className="block">
+                  <button className="button is-success is-pulled-right">
+                    Enviar
+                  </button>
                 </div>
               </div>
-              <div className='block'>
-                <button className='button is-success is-pulled-right'>
-                  Enviar
-                </button>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
+          );
+        })
+      ) : (
+        <p className="notification">
+          No hay actividades por realizar en este momento
+        </p>
+      )}
 
-      <div className='box'>
-        <div className='level'>
-          <div className='level-left'>
+      <div className="box">
+        <div className="level">
+          <div className="level-left">
             <div>
-              <h1 className='title'>Estimación de costos</h1>
+              <h1 className="title">Estimación de costos</h1>
               <p>Aquí iría otra descripción muy buena</p>
               <p>Fecha de cierre: 20/01/2022</p>
               <p>Tiempo restante: 2 días</p>
             </div>
           </div>
-          <div className='level-right'>
-            <button className='button is-static'>
+          <div className="level-right">
+            <button className="button is-static">
               <span class="icon">
                 <i class="fas fa-check"></i>
               </span>
@@ -65,7 +87,7 @@ const DeliverablesView = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default DeliverablesView;
